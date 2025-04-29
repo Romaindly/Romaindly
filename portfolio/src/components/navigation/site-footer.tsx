ef1d827a4017867f3c2cc54256191b2a74f06dbc
Romaindly <romain.delannoy@gastonberger.fr>
2025-04-29 16:43:38 +0200

    little update

--- portfolio/src/components/navigation/site-footer.tsx
+++ portfolio/src/components/navigation/site-footer.tsx
@@ -6,12 +6,12 @@ export default function SiteFooter() {
       <div className="container max-w-6xl flex flex-col items-center justify-between gap-4 md:h-20 md:flex-row">
         <div className="text-balance text-center text-sm font-medium leading-loose text-muted-foreground md:text-left">
           © {new Date().getFullYear()} {siteConfig.profile.name}. All rights reserved. Built by <a
-          href="https://github.com/MrInspection"
+          href="https://github.com/Romaindly"
           target="_blank"
           rel="noreferrer"
           className="font-medium underline underline-offset-4"
         >
-          Moussa
+          Romain
         </a>.
         </div>
       </div>
